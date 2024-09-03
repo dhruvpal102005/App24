ed18505f0a64b1fa6c9be4ee2d888da27375b7fe
Dhruv <dhruvpal102005@gmail.com>
2024-09-03 23:58:10 +0530

    added schedule

---
+++ app/lib/widget/upcoming_schedule.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class UpcomingSchedule extends StatelessWidget {
+  const UpcomingSchedule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "About Hospital",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(height: 15),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 2,
+                ),
+              ],
+            ),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      "Hospital Name",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text("Concern"),
+                    trailing: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage("images/hospital-1.jpg"),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Divider(
+                      thickness: 1,
+                      height: 20,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.calendar_month,
+                            color: Colors.black54,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "04/09/2024",
+                            style: TextStyle(
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.access_time_filled,
+                            color: Colors.black54,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "04:30 PM",
+                            style: TextStyle(
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "Confirmed",
+                            style: TextStyle(
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
